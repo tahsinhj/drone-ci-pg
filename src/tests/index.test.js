29bb6979c6a21859b32fa3955020936f2e34b5b0
Tahsin <tahsingenuies@gmail.com>
2021-09-25 15:06:28 +0700

    bypass test

---
+++ src/tests/index.test.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+const app = require("../app");
+
+const main = async () => {
+  // const res = await request(app).get("/");
+  // console.log(res.body);
+};
+
+main();
