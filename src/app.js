eced9bd82ca9e4f8f799572063ea541b133910d9
Tahsin <72777884+tahsinhj@users.noreply.github.com>
2021-10-03 21:36:24 +0700

    3rd

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ success: true, msg: "hf-2" });
+  res.json({ success: true, msg: "hf-3" });
 });
 
 module.exports = app;
