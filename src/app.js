6d81a6324007ea9a799b0ddda0457f06f02be986
Tahsin <72777884+tahsinhj@users.noreply.github.com>
2021-10-03 21:31:55 +0700

    add msg

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ success: true });
+  res.json({ success: true, msg: "hf-1" });
 });
 
 module.exports = app;
